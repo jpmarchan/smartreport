7fb1df1cbf6ba3a89bd4853c470640f8cf805af3
DESKTOP-8HJGQKO\mel <yenner.marchan.zamora@gmail.com>
2021-05-09 01:44:37 -0500

    integration auth firebase 70 %

--- app/src/main/java/com/example/smartreports/ui/LoginActivity.kt
+++ app/src/main/java/com/example/smartreports/ui/LoginActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.smartreports.MainActivity
 import com.example.smartreports.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -23,34 +25,52 @@ class LoginActivity : AppCompatActivity() {
         pass = findViewById(R.id.txt_pass)
         btn = findViewById(R.id.sign_button)
         sign()
+
     }
 
     fun sign(){
 
-        if (user.text.toString().isEmpty() || pass.text.toString().isEmpty()) {
-            Toast.makeText(this, "Debe completar los campos", Toast.LENGTH_SHORT).show()
-        }
+
 
         btn.setOnClickListener {
-            if(user.text.toString() == "yennerjp" && pass.text.toString() == "123456"
-                || user.text.toString() == "harol" && pass.text.toString() == "654321") {
-                Toast.makeText(this, "ingresando", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
 
-                /*Seteamos los flags al intent para que inicie una nueva actividad
-                sin posibilidad de regresar al activity anterior*/
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            if (user.text.toString().isNotEmpty() && pass.text.toString().isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(
+                        user.text.toString(),
+                        pass.text.toString()
+                    ).addOnCompleteListener {
+                        print(it.isSuccessful)
+                        if (it.isSuccessful) {
 
-                //Iniciamos la actividad mediante el intent
-                startActivity(intent)
+                            Toast.makeText(this, "ingresando", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            /*Seteamos los flags al intent para que inicie una nueva actividad
+                             sin posibilidad de regresar al activity anterior*/
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
-            }else{
-                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                            //Iniciamos la actividad mediante el intent
+                            startActivity(intent)
+                        } else {
+                            showAlert()
+                        }
+                    }
             }
+
         }
 
     }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autentificando al usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 
 }

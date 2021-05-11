ec63d40a16d5295e78ad0e41aa3565d3bf1c53df
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-05-10 22:08:27 -0500

    validation in email adress

--- app/src/main/java/com/example/smartreports/ui/LoginActivity.kt
+++ app/src/main/java/com/example/smartreports/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.smartreports.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -16,6 +17,8 @@ class LoginActivity : AppCompatActivity() {
     lateinit var user: EditText
     lateinit var pass: EditText
     lateinit var btn: Button
+    var emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,39 +32,42 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun sign(){
-
-
-
         btn.setOnClickListener {
-
             if (user.text.toString().isNotEmpty() && pass.text.toString().isNotEmpty()) {
-                FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(
-                        user.text.toString(),
-                        pass.text.toString()
-                    ).addOnCompleteListener {
-                        print(it.isSuccessful)
-                        if (it.isSuccessful) {
-
-                            Toast.makeText(this, "ingresando", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(this, MainActivity::class.java)
-                            /*Seteamos los flags al intent para que inicie una nueva actividad
-                             sin posibilidad de regresar al activity anterior*/
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-                            //Iniciamos la actividad mediante el intent
-                            startActivity(intent)
-                        } else {
-                            showAlert()
+                if (user.text.toString().trim { it <= ' ' }.matches(emailPattern.toRegex())) {
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(
+                            user.text.toString(),
+                            pass.text.toString()
+                        ).addOnCompleteListener {
+                            Log.d("tag", it.isSuccessful.toString())
+                            if (it.isSuccessful) {
+                                Toast.makeText(this, "ingresando", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, MainActivity::class.java)
+                                /*Seteamos los flags al intent para que inicie una nueva actividad
+                                 sin posibilidad de regresar al activity anterior*/
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                //Iniciamos la actividad mediante el intent
+                                startActivity(intent)
+                            } else {
+                                showAlert()
+                            }
                         }
-                    }
+                }else{
+                    Toast.makeText(applicationContext, "valid email address", Toast.LENGTH_SHORT).show()
+                }
+
+            }else {
+                Toast.makeText(this, "Ingrese los datos", Toast.LENGTH_SHORT).show()
             }
 
         }
 
     }
+
+
 
     private fun showAlert() {
         val builder = AlertDialog.Builder(this)
